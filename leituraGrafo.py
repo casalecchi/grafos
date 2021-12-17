a5294f39cb57e268b134d20aa24ad3da28b41947
Felipe Casalecchi <felipevilela191@gmail.com>
2021-12-17 14:37:23 -0300

    leitura_grafo criado.

---
+++ leituraGrafo.py
@@ -0,0 +1,20 @@
+from matrizAdjacencia import Grafo
+
+
+def ler_grafo(file):
+    vertices = 0
+    arestas = []
+    with open(file) as f:
+        for line in f.readlines():
+            data = line.split()
+            for i in range(len(data)):
+                data[i] = int(data[i])
+            if len(data) == 2:
+                arestas.append(data)
+            elif len(data) == 1:
+                vertices = data[0]
+    f.close()
+    g = Grafo(vertices)
+    for aresta in arestas:
+        g.adiciona_aresta(aresta[0], aresta[1])
+    return g
