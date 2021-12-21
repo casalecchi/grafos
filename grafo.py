a438ad24356be80cd7d25b1ccd6e9af85c3c6e40
Felipe Casalecchi <felipevilela191@gmail.com>
2021-12-21 17:46:23 -0300

    Lista de pais adicionada na dfs.

--- grafo.py
+++ grafo.py
@@ -15,14 +15,13 @@ class Grafo:
         pai = [-1 for _ in range(self.vertices)]
         vetor_marcacao[s - 1] = 1
         descobertos.append(s - 1)
-        nivel_atual = 1
         while descobertos:
             vertice = descobertos.pop(0)
             nivel_atual = nivel[vertice] + 1
             for index, vizinho in enumerate(self.grafo[vertice]):
-                if vizinho == 0:
-                    continue
                 if self.matriz:
+                    if vizinho == 0:
+                        continue
                     if vetor_marcacao[index] == 0:
                         vetor_marcacao[index] = 1
                         descobertos.append(index)
@@ -36,22 +35,27 @@ class Grafo:
                         pai[vizinho] = vertice + 1
             ordem.append(vertice + 1)
 
-        return ordem
+        return ordem, nivel, pai
 
     def dfs(self, s):
         vetor_marcacao = [0 for _ in range(self.vertices)]
         pilha = [s - 1]
         ordem = []
+        pai = [-1 for _ in range(self.vertices)]
         while len(pilha) != 0:
             vertice = pilha.pop()
             if vetor_marcacao[vertice] == 0:
                 vetor_marcacao[vertice] = 1
                 ordem.append(vertice + 1)
-                for i, vizinho in enumerate(self.grafo[vertice][::-1]):  # vértices descobertos em ordem decrescentes
-                    if vizinho == 0:
-                        continue
+                for index, vizinho in enumerate(self.grafo[vertice][::-1]):  # vértices descobertos em ordem decrescentes
                     if self.matriz:
-                        pilha.append(len(self.grafo[vertice]) - i - 1)
+                        if vizinho == 0:
+                            continue
+                        if len(self.grafo[vertice]) - index not in ordem:
+                            pai[len(self.grafo[vertice]) - index - 1] = vertice + 1
+                        pilha.append(len(self.grafo[vertice]) - index - 1)
                     if self.lista:
+                        if vizinho + 1 not in ordem:
+                            pai[vizinho] = vertice + 1
                         pilha.append(vizinho)
-        return ordem
+        return ordem, pai
