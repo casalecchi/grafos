05dafbe064bc7157958116a34a761da5f1757157
SylvioMello <69473039+SylvioMello@users.noreply.github.com>
2021-12-22 15:24:04 -0300

    Update gerar_saida.py

--- gerar_saida.py
+++ gerar_saida.py
@@ -3,6 +3,8 @@ from listaAdjacencia import grafo
 
 
 def gerar_saida(file, g):
+    """Função para escrever em um arquivo de texto o número de vértices, o número de arestas, o grau mínimo, o grau máximo, o grau médio, a mediana de grau
+    e os detalhes das componentes conexas"""
     with open(file, "w+") as f:
         linha = f"Número de vértices = {str(g.vertices)} \n" \
                 f"Número de arestas = {str(g.arestas)} \n" \
@@ -11,5 +13,11 @@ def gerar_saida(file, g):
                 f"Grau médio = {str(g.grau_medio())} \n" \
                 f"Mediana de grau = {str(g.mediana_de_grau())} \n"
         f.write(linha)
+        numcompconex = f"Número de Componentes Conexas = {str(g.componentes_conexas()[0])} \n"
+        f.write(numcompconex)
+        for i in range(len(g.componentes_conexas()[1])):
+            componentes = f"Componente {i + 1} = {str(g.componentes_conexas()[2][i])} \n" \
+                  f"Tamanho da componente {i + 1} = {str(g.componentes_conexas()[1][i])} \n"
+            f.write(componentes)
     f.close()
 gerar_saida("saida.txt", grafo)
