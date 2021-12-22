d68a1040879559b36f21650507a6f88d27b99211
SylvioMello <69473039+SylvioMello@users.noreply.github.com>
2021-12-22 13:47:52 -0300

    Update gerar_saida.py

--- gerar_saida.py
+++ gerar_saida.py
@@ -1,12 +1,15 @@
-from matrizAdjacencia import Grafo
+from matrizAdjacencia import grafo
+from listaAdjacencia import grafo
 
 
-def gerar_saida(file, g: Grafo):
-    with open(file, "r") as f:
-        linha = "Número de vértices = " + str(g.vertices) + "\n"
+def gerar_saida(file, g):
+    with open(file, "w+") as f:
+        linha = f"Número de vértices = {str(g.vertices)} \n" \
+                f"Número de arestas = {str(g.arestas)} \n" \
+                f"Grau mínimo = {str(g.grau_minimo())} \n" \
+                f"Grau máximo = {str(g.grau_maximo())} \n" \
+                f"Grau médio = {str(g.grau_medio())} \n" \
+                f"Mediana de grau = {str(g.mediana_de_grau())} \n"
         f.write(linha)
-        linha = "Número de arestas = " # implementar contador de arestas na classe do Grafo
-        f.write(linha)
-        # adicionar grau mínimo, máximo, médio e mediana de grau
-        # adicionar informações sore componentes conexas
     f.close()
+gerar_saida("saida.txt", grafo)
